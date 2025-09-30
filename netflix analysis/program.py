2c81217d11171e004c33da88a3b1222b36ca8654
Tejas Raj <tejasraj.da@gmail.com>
2025-09-30 11:21:57 +0530

    project files uploaded

---
+++ netflix analysis/program.py
@@ -0,0 +1,22 @@
+# number=int(input("Enter the Positive Number:"))
+# if number%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# num=int(input("Enter a Number:"))
+# if num>0:
+#     print("Positive Number")
+# elif num==0:
+#     print("Number is Zero")
+# else:
+#     print("Number is Negative")
+
+# year=int(input("Enter the Year:"))
+# if (year%4==0 and year%100!=0)or(year%400==0):
+#     print("is a leap year")
+# else:
+#     print("not a leap year")
+
+
+    
